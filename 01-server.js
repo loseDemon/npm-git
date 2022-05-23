f84d5b1a93cdd74f5943ad07aed42a2cbcd02b92
wwx <wwx_0504@163.com>
2022-05-23 20:29:09 +0800

    更改了输出内容

--- 01-server.js
+++ 01-server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const loadsh = require('loadsh');
 const app = express()
 
 app.get('/', function(req, res) {
-    res.send('<h1>Hello World</h1>')
+    res.send('<h1>Hello World,welcome to express</h1>');
+
 })
 
 app.listen(3000);
